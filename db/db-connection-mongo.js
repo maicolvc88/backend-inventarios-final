ad221c93eade3be57e886b37ca201914c7fef4a5
Maycol Vasquez <maicolvc88@gmail.com>
2022-10-25 11:00:41 -0500

    Se actualiza conexión a base de datos

--- db/db-connection-mongo.js
+++ db/db-connection-mongo.js
@@ -3,9 +3,13 @@ const mongoose = require('mongoose');
 const getConnection = async () => {
 
     try{
-        const MONGO_URI = 'mongodb+srv://root:qggvmJkaXVx4iIzB@cluster1.eyiym1l.mongodb.net/?retryWrites=true&w=majority';
+        /*const MONGO_URI = 'mongodb+srv://root:qggvmJkaXVx4iIzB@cluster1.eyiym1l.mongodb.net/?retryWrites=true&w=majority';
 
-        await mongoose.connect(MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI);*/
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true
+        });
     
         console.log('Conexión exitosa');
 
